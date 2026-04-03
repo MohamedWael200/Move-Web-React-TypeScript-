5a9f56966b0087bc619e1000b1aa3bb3fee335cd
MOHAMED <mohamedwaelll333@gmail.com>
2026-04-03 22:24:35 +0200

    Home Comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import {useMoves} from "./service/movesApi.ts";
+import Hero from "./pages/home/Hero.tsx";
+import Slider from "./pages/home/Slider.tsx";
+import type {moveData} from "./types/moveData.ts";
+
 function App() {
-  return (
+    const {moves , loading} = useMoves();
+    const topRateMoves : moveData[] = moves.filter((move) => move.rating > 7    )
+
+    // Filter Action moves
+    const actionMovies: moveData[] = moves.filter((move) =>
+        move.categories.some((cat) => cat.name === "Action")
+    );
+    //Filter Animation Moves
+    const animationMovies: moveData[] = moves.filter((move) =>
+        move.categories.some((cat) => cat.name === "Animation")
+    );
+    //Filter Animation Moves
+    const romanticMovies: moveData[] = moves.filter((move) =>
+        move.categories.some((cat) => cat.name === "Romantic")
+    );
+    return (
       <>
-        <h1>Home</h1>
+          <Hero />
+          <Slider moves={topRateMoves} loading={loading} title="Top Rate Moves" />
+          <Slider moves={actionMovies} loading={loading} title="Action Moves" />
+          <Slider moves={romanticMovies} loading={loading} title="Romantic Moves" />
+          <Slider moves={animationMovies} loading={loading} title="Animation Moves" />
       </>
   )
 }
